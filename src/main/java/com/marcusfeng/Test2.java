ff59215f2102f97b6596265596962b52e923ca83
marcusfeng <951115439@qq.com>
2019-07-19 19:57:33 +0800

    test

--- src/main/java/com/marcusfeng/Test2.java
+++ src/main/java/com/marcusfeng/Test2.java
@@ -2,6 +2,6 @@ package com.marcusfeng;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("marcusfeng test 2");
+        System.out.println("marcusfeng test error");
     }
 }
